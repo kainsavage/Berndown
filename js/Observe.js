187ee32cde55a596eb4037cef0fd3554e7b0e536
kainsavage <kainsavage@yahoo.com>
2016-04-22 14:09:52 -0700

    Default values of callbacks now empty callbacks
    Default values are hot. No need to check that the callback
    exists and is a function; make it a default value of an
    empty function and call it. If the dev passes a non-function,
    he will get a nice error message.

--- js/Observe.js
+++ js/Observe.js
@@ -19,20 +19,16 @@
  *   foo.bar = 1; // logs 'set'
  *   let baz = foo.bar; // logs 'get'
  */
-export function observable(obj,name,setter,getter) {
+export function observable(obj,name,setter=()=>{},getter=()=>{}) {
   let state = 0;
   Object.defineProperty(obj,name,{
     set(newv) {
       let oldv = state;
       state = newv;
-      if(setter && typeof setter === 'function') {
-        setter(oldv,newv);
-      }
+      setter(oldv,newv);
     }, 
     get() {
-      if(getter && typeof getter === 'function') {
-        getter();
-      }
+      getter();
       return state; 
     }
   });
@@ -59,7 +55,7 @@ export function observable(obj,name,setter,getter) {
  *   foo.bar.push('test'); // logs 'test'
  *   foo.bar.remove('test'); // logs 'test'
  */
-export function observableArray(obj,name,adder,subtracter) {
+export function observableArray(obj,name,adder=()=>{},subtracter=()=>{}) {
   obj[name] = new ObservableArray(adder,subtracter);
 }
 
@@ -93,17 +89,13 @@ class ObservableArray extends Array {
 
   pop() {
     let toRet = super.pop();
-    if(this.subtracter && typeof this.subtracter === 'function') {
-      this.subtracter(toRet);
-    }
+    this.subtracter(toRet);
     return toRet;
   }
 
   push(...vals) {
     super.push(...vals);
-    if(this.adder && typeof this.adder === 'function') {
-      this.adder(...vals);
-    }
+    this.adder(...vals);
   }
 
   remove(obj) {
